6d577484db087f78d7ef7d1172fdc8b6f7e53952
Ranuka Perera <random@sawrc.com>
2015-12-17 08:00:08 +0530

    add addCotacts and get Contacts

--- src/AddressBook.js
+++ src/AddressBook.js
@@ -4,3 +4,7 @@ function AddressBook() {
 AddressBook.prototype.addContact = function (contact) {
     this.contacts.push(contact);
 }
+AddressBook.prototype.getContact = function (contact_number) {
+    return this.contacts[contact_number];
+}
+
