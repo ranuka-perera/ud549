9419bbab54c3e2f2f4ae95a90f2ade1797744971
Ranuka Perera <random@sawrc.com>
2015-12-17 08:14:45 +0530

    Add Delete function. Use beforeEach for Addressbook spec.

--- spec/AddressBookSpec.js
+++ spec/AddressBookSpec.js
@@ -1,10 +1,26 @@
 describe('Address Book', function() {
+    var addressBook, thisContact;
+    
+    beforeEach(function() {
+        addressBook = new AddressBook();
+        thisContact = new Contact();
+    });
     it('should be able to add a new contact', function() {
-        var addressBook = new AddressBook(),
-            thisContact = new Contact();
-            
+
         addressBook.addContact(thisContact);
         
         expect(addressBook.getContact(0)).toBe(thisContact);
+    });
+    it('should be able to delete a contact', function() {
+        var otherContact = new Contact();
+
+        addressBook.addContact(thisContact);
+        addressBook.addContact(otherContact);
+        
+        addressBook.deleteContact(0);
+        expect(addressBook.getContact(0)).toBe(otherContact);
+        
+        addressBook.deleteContact(0);
+        expect(addressBook.getContact(0)).not.toBeDefined();
     });
 });
--- src/AddressBook.js
+++ src/AddressBook.js
@@ -7,4 +7,6 @@ AddressBook.prototype.addContact = function (contact) {
 AddressBook.prototype.getContact = function (contact_number) {
     return this.contacts[contact_number];
 }
-
+AddressBook.prototype.deleteContact = function (contact_number) {
+    this.contacts.splice(contact_number, 1);
+}
